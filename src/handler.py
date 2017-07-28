5841595c91a072d61406b7efb3348163a9d790fe
Eric Romrell <eric_romrell@byu.edu>
2017-07-28 10:28:44 -0600

    Added database connection

--- src/handler.py
+++ src/handler.py
@@ -2,6 +2,20 @@ from service_exception import ServiceException
 from manager import Manager
 
 import json
+import sys
+import properties
+import pymysql
+
+rds_host = properties.db_host
+name = properties.db_username
+password = properties.db_password
+db_name = properties.db_name
+
+try:
+    conn = pymysql.connect(rds_host, user = name, passwd = password, db = db_name, connect_timeout = 5)
+except Exception as e:
+    print("ERROR: Could not connect to MySQL", e)
+    sys.exit()
 
 def lambda_handler(event, context):
     try:
@@ -17,7 +31,7 @@ def lambda_handler(event, context):
             response_body = manager.get_brackets()
 
         # TODO: Fill in more endpoints here
-        
+
         else:
             raise ServiceException("Invalid path: '{} {}'".format(resource, method))
 
