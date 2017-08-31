c7b2abc582f975a3c2eb26d841270456cbb78d51
Mark Roth <mzroth@gmail.com>
2017-08-31 12:42:56 -0600

    started moving tests around

---
+++ test/manager_tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+import da
+from manager import Manager
+
+from res import properties
+
+class ManagerTest(unittest.TestCase):
+    def setUp(self):
+        manager = Manager("test_fqxpeow_user@tfbnw.net")
