0847c36aaf194995ffc6f7ffd5f3889830a68742
Eric Romrell <eric_romrell@byu.edu>
2018-02-10 15:59:42 -0700

    Fixed da tests

--- test/integration_tests/da_tests.py
+++ test/integration_tests/da_tests.py
@@ -24,7 +24,7 @@ class DaTest(unittest.TestCase):
 
     def test_tournaments(self):
         dict1 = {"name": "test"}
-        dict2 = {"name": "test", "master_bracket_id": 0, "image_url": "test.com", "active": 1}
+        dict2 = {"name": "test", "master_bracket_id": 0, "draws_url": "test_draws.com", "image_url": "test_image.com", "active": 1}
         self.run_simple_tests(da.get_tournaments, da.create_tournament, da.update_tournament, da.delete_tournament, "tournament_id", dict1, dict2)
 
     def test_brackets(self):
